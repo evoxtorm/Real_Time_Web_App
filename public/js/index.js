a0b98e55aca309183dfcbe7c2ec7a2d37455c64f
evoxtorm <evostorm96@gmail.com>
2018-03-18 00:33:13 +0530

    Add newMessage and createMessage events

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,28 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server');
+
+// socket.emit('createEmail', {
+//   to: 'evo@outlook.com',
+//   text: 'Hey, this is important message'
+// });
+
+socket.emit('createMessage', {
+  from:'Hitesh',
+  text: 'We recieved your message'
+});
+
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+
+// socket.on('newEmail', function (email) {
+//   console.log('New email', email);
+// });
+
+socket.on('newMessage', function (Message) {
+  console.log('New message', Message);
 });
--- server/server.js
+++ server/server.js
@@ -15,6 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'hola@gmail.com',
+  //   text: 'Hey, where are you',
+  //   createAt: 12
+  // });
+
+  socket.emit('newMessage', {
+    from: 'Evo',
+    text: 'Hope this is better',
+    createdAt: 12
+  });
+
+// socket.on('createEmail', (newEmail) => {
+//   console.log('createEmail', newEmail);
+// });
+
+socket.on('createMessage', (message) => {
+  console.log('new Message',message );
+});
 
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
