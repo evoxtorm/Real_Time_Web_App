71dec6001d1033829ad9fa777bfd25e35390d9a6
evoxtorm <evostorm96@gmail.com>
2018-03-17 23:54:32 +0530

    Added Index.js

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+var socket = io();
+
+
+socket.on('connect', function () {
+  console.log('connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
